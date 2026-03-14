f1f1c355e62ae1074d0452de4e6d6162a839573b
Student Deep Prasad <deep7578048478@gmail.com>
2026-03-14 16:31:10 +0530

    regestration and login functionality

---
+++ blog_main/forms.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import User
+
+from django.contrib.auth.forms import UserCreationForm
+
+
+class RegisterForm(UserCreationForm):
+    
+    class Meta:
+        model = User
+        fields = ('email','username', 'password1', 'password2')
+
+    # def save(self, commit=True):
+    #     user = super().save(commit=False)
+    #     user.email = self.cleaned_data['email']
+    #     if commit:
+    #         user.save()
+    #     return user
+    
+    
+    
