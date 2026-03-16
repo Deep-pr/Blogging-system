2bd4469e49c828bbe647529d9f2ae7711cf0a26f
Student Deep Prasad <deep7578048478@gmail.com>
2026-03-17 02:29:27 +0530

    comment system

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,14 +1,12 @@
 from django.contrib import admin
-from blogs.models import Blog
-from blogs.models import Category
-
-
+from .models import Category, Blog, Comment
 
 class BlogAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
-    list_display = ('title', 'author', 'category', 'status', 'is_featured', 'created_at')
-    search_fields = ('id', 'title', 'status', 'author__username', 'category__category_name')
+    list_display = ('title', 'category', 'author', 'status', 'is_featured')
+    search_fields = ('id', 'title', 'category__category_name', 'status')
     list_editable = ('is_featured',)
 
 admin.site.register(Category)
 admin.site.register(Blog, BlogAdmin)
+admin.site.register(Comment)
--- blogs/views.py
+++ blogs/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
-from .models import Blog, Category
+from .models import Blog, Category, Comment
 from django.db.models import Q
+from django.http import HttpResponseRedirect
 
 
 def posts_by_category(request, category_id):
@@ -20,8 +21,22 @@ def posts_by_category(request, category_id):
 
 def blogs(request, slug):
     single_blog = get_object_or_404(Blog, slug=slug, status='Published')
+    if request.method == 'POST':
+        comment = Comment()
+        comment.user = request.user
+        comment.blog = single_blog
+        comment.comment = request.POST['comment']
+        comment.save()
+        return HttpResponseRedirect(request.path_info)
+
+    # Comments
+    comments = Comment.objects.filter(blog=single_blog)
+    comment_count = comments.count()
+    
     context = {
         'single_blog': single_blog,
+        'comments': comments,
+        'comments_count': comment_count,
     }
     return render(request, 'blogs.html', context   )
 
